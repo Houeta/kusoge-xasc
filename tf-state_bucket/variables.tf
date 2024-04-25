647ebf5ee4cafa318d4c2f440e2702e9ee44f7f3
miracle <miracle1695@gmail.com>
2024-04-25 15:06:34 +0300

    Create s3 backend

---
+++ tf-state_bucket/variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+  description = "Project name"
+  type = string
+}
+
+variable "profile" {
+  type = string
+  default = ""
+}
+
+variable "region" {
+  type = string
+}
+
+variable "username" {
+  description = "Owner's username"
+  type = string
+}
+
+variable "tags" {
+  type = map(any)
+  default = {}
+}
